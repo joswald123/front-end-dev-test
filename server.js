bacacc98dbbdeeb4e849c1813082f5ab4103cdd7
joswald123 <48632836+joswald123@users.noreply.github.com>
2020-07-09 22:12:14 -0700

    added server file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
