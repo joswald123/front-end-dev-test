55fd7684bd9ef6d9007a0c37f61b664aabf77471
joswald123 <48632836+joswald123@users.noreply.github.com>
2020-07-09 23:48:36 -0700

    updated server file

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const PORT = process.env.PORT || 3001;
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express'});
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+    );
+});
+
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
